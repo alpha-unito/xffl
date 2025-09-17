9b774a08655995657594c15e7fa4399f94585c2b
Gianluca Mittone <gianluca.mitttone@unito.it>
2025-09-17 09:34:19 +0200

    Plotting code upated

--- examples/simulation/03_LLM/plot/plot_NCCL.py
+++ examples/simulation/03_LLM/plot/plot_NCCL.py
@@ -2,11 +2,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+
+# -- General settings --
+FONT_SIZE = 10
+
 sns.set_theme(style="darkgrid")
 
-plt.rc("xtick", labelsize=12)
-plt.rc("ytick", labelsize=12)
+plt.rc("xtick", labelsize=FONT_SIZE)
+plt.rc("ytick", labelsize=FONT_SIZE)
 
+
+# -- Data extraction --
 logs = "/leonardo_scratch/fast/uToID_bench/xffl/examples/simulation/03_LLM/logs"
 
 model = "llama3.1-8b"
@@ -107,385 +113,269 @@ data_coalesced_optimized_ = data[
     & (data["Contiguous Memory"] == False)
 ]
 
-fig, ax = plt.subplots(
-    nrows=6,
-    ncols=2,
-    sharex="all",
-    figsize=(10, 10),
-)
-
+# -- Subplot ---
 
 xticks = [2, 4, 8, 16, 32, 64, 128]
 yticks = [80, 90, 100, 110, 120]
-x_lim = (1.98, 129)
-y_lim = (84, 120)
+ytickslabels = [80, 90, 100, 110, 120]
+x_lim = (2, 128)
+y_lim = (80, 120)
 
+fig, ax = plt.subplots(
+    nrows=6,
+    ncols=2,
+    sharex=True,
+    sharey=True,
+    figsize=(10, 9),
+    dpi=300,
+    layout="tight",
+    subplot_kw={
+        "xlabel": None,
+        "ylabel": None,
+        "xscale": "log",
+        "ylim": y_lim,
+        "yticks": yticks,
+        "yticklabels": ytickslabels,
+        "xlim": x_lim,
+        "xticks": xticks,
+        "xticklabels": xticks,
+    },
+)
+fig.supylabel("Throughput (Gb/s)", fontsize=14, y=0.45)
+fig.supxlabel("Number of nodes", fontsize=14, x=0.75, y=0.03)
+
+ax[0][0].set_title("With optimizations (multiple streams + contiguous memory)")
+ax[0][1].set_title("Without optimizations")
+
+ax[0][0].set_ylabel("Layer")
+ax[1][0].set_ylabel("Layer opt.")
+ax[2][0].set_ylabel("Bucket")
+ax[3][0].set_ylabel("Bucket opt.")
+ax[4][0].set_ylabel("Coalesced")
+ax[5][0].set_ylabel("Coalesced opt.")
+
+hue_order = [
+    "ring",
+    "tree",
+    "collnet",
+    "collnetchain",
+    "collnetdirect",
+    "nvls",
+    "nvlstree",
+    "pat",
+]
 style_order = [
-    "layer_by_layer",
-    "layer_by_layer_optimized",
-    "bucket_flatten",
-    "bucket_optimized_flatten",
-    "bucket_coalesced",
-    "bucket_optimized_coalesced",
+    "LL",
+    "LL128",
+    "SIMPLE",
 ]
-order = [
-    "layer_by_layer",
-    "layer_by_layer_optimized",
-    "bucket_flatten",
-    "bucket_optimized_flatten",
-    "bucket_coalesced",
-    "bucket_optimized_coalesced",
-]
-labels = [
-    "Layer",
-    "Layer\noptimized",
-    "Bucket",
-    "Bucket\noptimized",
-    "Coalesced",
-    "Coalesced\noptimized",
-]
+palette = dict(
+    zip(
+        hue_order,
+        sns.color_palette(palette="husl", n_colors=8),
+    )
+)
 
-palette = {
-    (False, False): "limegreen",
-    (False, True): "green",
-    (True, False): "lightskyblue",
-    (True, True): "dodgerblue",
-}
+# -- Plotting --
 
 fig1 = sns.lineplot(
     data=data_layer,
     x="nodes",
     y="Real Throughput (Gb/s)",
-    hue=data_layer[["NCCL Algo", "NCLL Proto"]].apply(tuple, axis=1),
-    # hue_order=palette.keys(),
-    # style="Aggregation Strategy",
-    # style_order=style_order,
+    hue="NCCL Algo",
+    hue_order=hue_order,
+    palette=palette,
+    style="NCLL Proto",
     legend="full",
-    # palette=palette,
-    # err_style="bars",
-    # errorbar=("pi", 100),
     sort=True,
     ax=ax[0][0],
 )
-fig1.set(
-    ylabel="Layer",
-    ylim=y_lim,
-)
-# fig1.set_xticks([], minor=True)
-# fig1.set_xticks(ticks=xticks)
-ax[0][0].set_title("With optimizations (streams/contiguous)")
-fig1.grid(True, axis="both")
 
 fig2 = sns.lineplot(
     data=data_layer_optimized,
     x="nodes",
     y="Real Throughput (Gb/s)",
-    hue=data_layer_optimized[["NCCL Algo", "NCLL Proto"]].apply(tuple, axis=1),
-    # hue_order=palette.keys(),
-    # style="Aggregation Strategy",
-    # style_order=style_order,
-    # palette=palette,
-    err_style="bars",
-    errorbar=("pi", 100),
+    hue="NCCL Algo",
+    hue_order=hue_order,
+    palette=palette,
+    style="NCLL Proto",
+    style_order=style_order,
     legend=None,
     sort=True,
     ax=ax[1][0],
 )
-fig2.set(
-    ylabel="Layer optimized",
-    ylim=y_lim,
-    xlim=x_lim,
-    xlabel=None,
-    xscale="log",
-    xticks=xticks,
-    xticklabels=xticks,
-)
-fig2.grid(True, axis="both")
 
 fig3 = sns.lineplot(
     data=data_bucket,
     x="nodes",
     y="Real Throughput (Gb/s)",
-    hue=data_bucket[["NCCL Algo", "NCLL Proto"]].apply(tuple, axis=1),
-    # hue_order=palette.keys(),
-    # style="Aggregation Strategy",
-    # style_order=style_order,
-    # palette=palette,
-    err_style="bars",
-    errorbar=("pi", 100),
+    hue="NCCL Algo",
+    hue_order=hue_order,
+    palette=palette,
+    style="NCLL Proto",
+    style_order=style_order,
     legend=None,
     sort=True,
     ax=ax[2][0],
 )
-fig3.set(
-    ylabel="Bucket",
-    ylim=y_lim,
-    xlim=x_lim,
-    xlabel=None,
-    xscale="log",
-    xticks=xticks,
-    xticklabels=xticks,
-)
-fig3.grid(True, axis="both")
 
 fig4 = sns.lineplot(
     data=data_bucket_optimized,
     x="nodes",
     y="Real Throughput (Gb/s)",
-    hue=data_bucket_optimized[["NCCL Algo", "NCLL Proto"]].apply(tuple, axis=1),
-    # hue_order=palette.keys(),
-    # style="Aggregation Strategy",
-    # style_order=style_order,
-    # palette=palette,
-    err_style="bars",
-    errorbar=("pi", 100),
+    hue="NCCL Algo",
+    hue_order=hue_order,
+    palette=palette,
+    style="NCLL Proto",
+    style_order=style_order,
     legend=None,
     sort=True,
     ax=ax[3][0],
 )
-fig4.set(
-    ylabel="Bucket optimized",
-    ylim=y_lim,
-    xlim=x_lim,
-    xlabel=None,
-    xscale="log",
-    xticks=xticks,
-    xticklabels=xticks,
-)
-fig4.grid(True, axis="both")
 
 fig5 = sns.lineplot(
     data=data_coalesced,
     x="nodes",
     y="Real Throughput (Gb/s)",
-    hue=data_coalesced[["NCCL Algo", "NCLL Proto"]].apply(tuple, axis=1),
-    # hue_order=palette.keys(),
-    # style="Aggregation Strategy",
-    # style_order=style_order,
-    # palette=palette,
-    err_style="bars",
-    errorbar=("pi", 100),
+    hue="NCCL Algo",
+    hue_order=hue_order,
+    palette=palette,
+    style="NCLL Proto",
+    style_order=style_order,
     legend=None,
     sort=True,
     ax=ax[4][0],
 )
-fig5.set(
-    ylabel="Coalesced",
-    ylim=y_lim,
-    xlim=x_lim,
-    xlabel=None,
-    xscale="log",
-    xticks=xticks,
-    xticklabels=xticks,
-)
-fig5.grid(True, axis="both")
 
 fig6 = sns.lineplot(
     data=data_coalesced_optimized,
     x="nodes",
     y="Real Throughput (Gb/s)",
-    hue=data_coalesced_optimized[["NCCL Algo", "NCLL Proto"]].apply(tuple, axis=1),
-    # hue_order=palette.keys(),
-    # style="Aggregation Strategy",
-    # style_order=style_order,
-    # palette=palette,
-    err_style="bars",
-    errorbar=("pi", 100),
+    hue="NCCL Algo",
+    hue_order=hue_order,
+    palette=palette,
+    style="NCLL Proto",
+    style_order=style_order,
     legend=None,
     sort=True,
     ax=ax[5][0],
 )
-fig6.set(
-    ylabel="Coalesced optimized",
-    ylim=y_lim,
-    xlim=x_lim,
-    xlabel=None,
-    xscale="log",
-    xticks=xticks,
-    xticklabels=xticks,
-)
-fig6.grid(True, axis="both")
 
+# ----- Secondo column -----
 
 fig7 = sns.lineplot(
     data=data_layer_,
     x="nodes",
     y="Real Throughput (Gb/s)",
-    hue=data_layer_[["NCCL Algo", "NCLL Proto"]].apply(tuple, axis=1),
-    # hue_order=palette.keys(),
-    # style="Aggregation Strategy",
-    # style_order=style_order,
+    hue="NCCL Algo",
+    hue_order=hue_order,
+    palette=palette,
+    style="NCLL Proto",
+    style_order=style_order,
     legend=None,
-    # palette=palette,
-    # err_style="bars",
-    # errorbar=("pi", 100),
     sort=True,
     ax=ax[0][1],
 )
-fig7.set(ylabel=None, ylim=y_lim, yticklabels=[])
-# fig1.set_xticks([], minor=True)
-# fig1.set_xticks(ticks=xticks)
-fig1.grid(True, axis="both")
-ax[0][1].set_title("Without optimizations (streams/contiguous)")
 
 fig8 = sns.lineplot(
     data=data_layer_optimized_,
     x="nodes",
     y="Real Throughput (Gb/s)",
-    hue=data_layer_optimized_[["NCCL Algo", "NCLL Proto"]].apply(tuple, axis=1),
-    # hue_order=palette.keys(),
-    # style="Aggregation Strategy",
-    # style_order=style_order,
-    # palette=palette,
-    err_style="bars",
-    errorbar=("pi", 100),
+    hue="NCCL Algo",
+    hue_order=hue_order,
+    palette=palette,
+    style="NCLL Proto",
+    style_order=style_order,
     legend=None,
     sort=True,
     ax=ax[1][1],
 )
-fig8.set(
-    ylabel=None,
-    ylim=y_lim,
-    xlim=x_lim,
-    xlabel=None,
-    xscale="log",
-    xticks=xticks,
-    xticklabels=xticks,
-    yticklabels=[],
-)
-fig8.grid(True, axis="both")
 
 fig9 = sns.lineplot(
     data=data_bucket_,
     x="nodes",
     y="Real Throughput (Gb/s)",
-    hue=data_bucket_[["NCCL Algo", "NCLL Proto"]].apply(tuple, axis=1),
-    # hue_order=palette.keys(),
-    # style="Aggregation Strategy",
-    # style_order=style_order,
-    # palette=palette,
-    err_style="bars",
-    errorbar=("pi", 100),
+    hue="NCCL Algo",
+    hue_order=hue_order,
+    palette=palette,
+    style="NCLL Proto",
+    style_order=style_order,
     legend=None,
     sort=True,
     ax=ax[2][1],
 )
-fig9.set(
-    ylabel=None,
-    ylim=y_lim,
-    xlim=x_lim,
-    xlabel=None,
-    xscale="log",
-    xticks=xticks,
-    xticklabels=xticks,
-    yticklabels=[],
-)
-fig9.grid(True, axis="both")
 
 fig10 = sns.lineplot(
     data=data_bucket_optimized_,
     x="nodes",
     y="Real Throughput (Gb/s)",
-    hue=data_bucket_optimized_[["NCCL Algo", "NCLL Proto"]].apply(tuple, axis=1),
-    # hue_order=palette.keys(),
-    # style="Aggregation Strategy",
-    # style_order=style_order,
-    # palette=palette,
-    err_style="bars",
-    errorbar=("pi", 100),
+    hue="NCCL Algo",
+    hue_order=hue_order,
+    palette=palette,
+    style="NCLL Proto",
+    style_order=style_order,
     legend=None,
     sort=True,
     ax=ax[3][1],
 )
-fig10.set(
-    ylabel=None,
-    ylim=y_lim,
-    xlim=x_lim,
-    xlabel=None,
-    xscale="log",
-    xticks=xticks,
-    xticklabels=xticks,
-    yticklabels=[],
-)
-fig10.grid(True, axis="both")
 
 fig11 = sns.lineplot(
     data=data_coalesced_,
     x="nodes",
     y="Real Throughput (Gb/s)",
-    hue=data_coalesced_[["NCCL Algo", "NCLL Proto"]].apply(tuple, axis=1),
-    # hue_order=palette.keys(),
-    # style="Aggregation Strategy",
-    # style_order=style_order,
-    # palette=palette,
-    err_style="bars",
-    errorbar=("pi", 100),
+    hue="NCCL Algo",
+    hue_order=hue_order,
+    palette=palette,
+    style="NCLL Proto",
+    style_order=style_order,
     legend=None,
     sort=True,
     ax=ax[4][1],
 )
-fig11.set(
-    ylabel=None,
-    ylim=y_lim,
-    xlim=x_lim,
-    xlabel=None,
-    xscale="log",
-    xticks=xticks,
-    xticklabels=xticks,
-    yticklabels=[],
-)
-fig11.grid(True, axis="both")
 
 fig12 = sns.lineplot(
     data=data_coalesced_optimized_,
     x="nodes",
     y="Real Throughput (Gb/s)",
-    hue=data_coalesced_optimized_[["NCCL Algo", "NCLL Proto"]].apply(tuple, axis=1),
-    # hue_order=palette.keys(),
-    # style="Aggregation Strategy",
-    # style_order=style_order,
-    # palette=palette,
-    err_style="bars",
-    errorbar=("pi", 100),
+    hue="NCCL Algo",
+    hue_order=hue_order,
+    palette=palette,
+    style="NCLL Proto",
+    style_order=style_order,
     legend=None,
     sort=True,
     ax=ax[5][1],
 )
-fig12.set(
-    ylabel=None,
-    ylim=y_lim,
-    xlim=x_lim,
-    xlabel=None,
-    xscale="log",
-    xticks=xticks,
-    xticklabels=xticks,
-    yticklabels=[],
-)
-fig12.grid(True, axis="both")
+
+for axis in ax.flat:
+    axis.set_xlabel(None)
 
 
 fig1.legend(
     title=None,
-    loc="upper right",
-    # handles=fig1.get_legend_handles_labels()[0],
-    # labels=["Optimization"]
-    # + [
-    #    "No optimizations",
-    #    "Contiguous\nMemory",
-    #    "Multiple\nCUDA Streams",
-    #    "Both",
-    # ]
-    # + ["\nAggregation algorithm"]
-    # + labels,
+    loc="center right",
+    handles=fig1.get_legend_handles_labels()[0],
+    labels=[
+        "NCCL Algorithm",
+        "Ring",
+        "Tree",
+        "Collnet",
+        "CollnetChain",
+        "CollnetDirect",
+        "NVLS",
+        "NVLSTree",
+        "PAT",
+        "NCLL Protocol",
+        "SIMPLE",
+        "LL",
+        "LL128",
+    ],
     ncol=1,
-    bbox_to_anchor=(2.6, 1),
+    bbox_to_anchor=(2.4, -2.3),
     fontsize=12,
 )
 
-
-fig.supylabel("Throughput (Gb/s)", fontsize=14, y=0.5)
-fig.supxlabel("Number of nodes", fontsize=14, x=0.75, y=0)
-plt.subplots_adjust(hspace=0.1, wspace=0.1, bottom=0.1, top=0.8, right=1.4)
+plt.subplots_adjust(hspace=0.3, wspace=0.05, bottom=0.1, top=0.8, right=1.4)
 fig.figure.savefig("out.png", bbox_inches="tight", dpi=300, format="png")
-# fig.figure.savefig("multiple-node.pdf", bbox_inches="tight", dpi=300, format="pdf")
+fig.figure.savefig("multiple-node.pdf", bbox_inches="tight", dpi=300, format="pdf")
 quit()
