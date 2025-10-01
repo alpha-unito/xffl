3c7c3f47b5c841444f188e989124d8635bf9759c
Gianluca Mittone <gianluca.mittone@unito.it>
2025-10-01 09:07:19 +0200

    Revise formatter

--- xffl/custom/formatter.py
+++ xffl/custom/formatter.py
@@ -1,4 +1,4 @@
-"""Custom logging.Formatter for formatted and coloured logging"""
+"""Custom logging.Formatter for formatted and coloured logging."""
 
 import logging
 from logging import LogRecord
@@ -6,14 +6,21 @@ from typing import Literal, Optional
 
 
 class CustomFormatter(logging.Formatter):
-    """Logging colored formatter, adapted from https://stackoverflow.com/a/56944256/3638629"""
+    """Logging formatter with color support per log level.
 
-    grey = "\x1b[38;20m"
-    blue = "\x1b[38;5;39m"
-    yellow = "\x1b[38;5;226m"
-    red = "\x1b[38;5;196m"
-    bold_red = "\x1b[31;1m"
-    reset = "\x1b[0m"
+    Colors adapted from https://stackoverflow.com/a/56944256/3638629.
+    """
+
+    # ANSI color codes
+    GREY = "\x1b[38;20m"  # Info
+    BLUE = "\x1b[38;5;39m"  # Debug
+    YELLOW = "\x1b[38;5;226m"  # Warning
+    RED = "\x1b[38;5;196m"  # Error
+    BOLD_RED = "\x1b[31;1m"  # Critical
+    RESET = "\x1b[0m"
+
+    DEFAULT_FORMAT = "%(asctime)s | %(name)16s | %(levelname)8s | %(message)s"
+    DATE_FORMAT = "%Y-%m-%d %H:%M:%S"  # Less detailed: only hours:minutes:seconds
 
     def __init__(
         self,
@@ -21,37 +28,45 @@ class CustomFormatter(logging.Formatter):
         datefmt: Optional[str] = None,
         style: Literal["%", "{", "$"] = "%",
         validate: bool = True,
-    ):
-        """Creation of the CustomFormatter.
+    ) -> None:
+        """Initialize the CustomFormatter.
 
-        :param fmt: format of the logged message.
+        :param fmt: Format string for log messages. Defaults to a structured format.
         :type fmt: Optional[str]
-        :param datefmt: format of the logged date.
+        :param datefmt: Date format string.
         :type datefmt: Optional[str]
-        :param style: style of the formatted logged messages.
-        :type style: str
-        :param validate: validate the logging style.
+        :param style: Style of the format string ("%", "{", or "$").
+        :type style: Literal["%", "{", "$"]
+        :param validate: Whether to validate the format string.
         :type validate: bool
         """
-        super().__init__(fmt, datefmt, style, validate)
+        super().__init__(
+            fmt=fmt or self.DEFAULT_FORMAT,
+            datefmt=datefmt or self.DATE_FORMAT,
+            style=style,
+            validate=validate,
+        )
 
-        self.fmt = "%(asctime)s | %(name)16s | %(levelname)8s | %(message)s"
+        self.fmt = fmt or self.DEFAULT_FORMAT
+
+        # Mapping log levels to colored formats
         self.FORMATS = {
-            logging.INFO: self.grey + self.fmt + self.reset,
-            logging.DEBUG: self.blue + self.fmt + self.reset,
-            logging.WARNING: self.yellow + self.fmt + self.reset,
-            logging.ERROR: self.red + self.fmt + self.reset,
-            logging.CRITICAL: self.bold_red + self.fmt + self.reset,
+            logging.DEBUG: self.BLUE + self.fmt + self.RESET,
+            logging.INFO: self.GREY + self.fmt + self.RESET,
+            logging.WARNING: self.YELLOW + self.fmt + self.RESET,
+            logging.ERROR: self.RED + self.fmt + self.RESET,
+            logging.CRITICAL: self.BOLD_RED + self.fmt + self.RESET,
         }
 
     def format(self, record: LogRecord) -> str:
-        """Format the provided record
+        """Format the provided log record with level-specific color.
 
-        :param record: Logging record
+        :param record: The log record to format.
         :type record: LogRecord
-        :return: Formatted logging record
+        :return: The formatted log message as a string.
         :rtype: str
         """
-
-        formatter = logging.Formatter(self.FORMATS.get(record.levelno))
+        formatter = logging.Formatter(
+            self.FORMATS.get(record.levelno, self.fmt), datefmt=self.DATE_FORMAT
+        )
         return formatter.format(record)
