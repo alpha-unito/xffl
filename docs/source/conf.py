696cb814142fd054b15279bc1ccb5f6694bb4076
Gianluca Mittone <gianluca.mittone@unito.it>
2025-10-03 10:46:14 +0200

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,6 +7,7 @@
 # add these directories to sys.path here.
 import sys
 from pathlib import Path
+from xffl.utils.constants import VERSION
 
 sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
 
@@ -14,20 +15,17 @@ sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "Cross-Facility Federated Learning"
-copyright = "2025, Gianluca Mittone, Alberto Mulone, Giulio Malenza, Robert Birke, Marco Aldinucci"
+copyright = "2024, Gianluca Mittone, Alberto Mulone, Giulio Malenza, Iacopo Colonnelli, Robert Birke, Marco Aldinucci"
 author = (
-    "Gianluca Mittone, Alberto Mulone, Giulio Malenza, Robert Birke, Marco Aldinucci"
+    "Gianluca Mittone, Alberto Mulone, Giulio Malenza, Iacopo Colonnelli, Robert Birke, Marco Aldinucci"
 )
-release = "0.2.0"
+release = VERSION
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     "sphinx.ext.duration",
-    # "sphinx.ext.doctest",
-    # "sphinx.ext.autodoc",
-    # "sphinx.ext.autosummary",
     "autoapi.extension",
 ]
 
