2a12672712d78d6b38db114fdce8f5644d04ff62
MeluXina user <u101522@mel2106-ib0.meluxina.lxp.lu>
2025-10-22 08:35:20 +0200

    Update Karolina script

--- xffl/workflow/scripts/facilities/meluxina.sh
+++ xffl/workflow/scripts/facilities/meluxina.sh
@@ -3,4 +3,4 @@
 # MELUXINA specific environment variables
 export XFFL_LOCAL_TMPDIR=${LOCALSCRATCH}
 
-module load Python/3.12.3-GCCcore-13.3.0 CUDA/12.6.0
+module load Python/3.11.10-GCCcore-13.3.0
