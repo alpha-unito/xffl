a855361edfa7d3968e75601ae0455b132c74f725
MeluXina user <u101522@login03.meluxina.lxp.lu>
2025-10-22 08:50:26 +0200

    Updated Meluxina script

--- xffl/workflow/scripts/facilities/meluxina.sh
+++ xffl/workflow/scripts/facilities/meluxina.sh
@@ -3,4 +3,4 @@
 # MELUXINA specific environment variables
 export XFFL_LOCAL_TMPDIR=${LOCALSCRATCH}
 
-module load Python/3.11.10-GCCcore-13.3.0
+module load Python/3.11.10-GCCcore-13.3.0 Apptainer/1.3.6-GCCcore-13.3.0
