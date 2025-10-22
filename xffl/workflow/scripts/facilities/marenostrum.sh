89dddecc1436887816823e517689b2368949e28a
Gianluca Mittone <gianluca.mittone@unito.it>
2025-10-22 15:59:11 +0200

    Fix MareNostrum facility script

--- xffl/workflow/scripts/facilities/marenostrum.sh
+++ xffl/workflow/scripts/facilities/marenostrum.sh
@@ -3,4 +3,4 @@
 # MARENOSTRUM5 specific environment variables
 export XFFL_LOCAL_TMPDIR=${TMPDIR}
 
-module load intel mkl impi hdf5 python/3.11.5-gcc
+module load intel mkl impi hdf5 python/3.11.5-gcc singularity/4.1.5
